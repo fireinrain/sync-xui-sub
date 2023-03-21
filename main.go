9f0146676d982f95af66ccd86029e5648227b8c2
sunrise <575563079@qq.com>
2023-03-21 15:26:29 +0800

    subconverter

--- main.go
+++ main.go
@@ -21,19 +21,4 @@ func main() {
 
 	fmt.Println(linksStr)
 
-	//let obj = {
-	//            v: '2',
-	//            ps: remark,
-	//            add: address,
-	//            port: this.port,
-	//            id: this.settings.vmesses[0].id,
-	//            aid: this.settings.vmesses[0].alterId,
-	//            net: network,
-	//            type: type,
-	//            host: host,
-	//            path: path,
-	//            tls: this.stream.security,
-	//        };
-	//        return 'vmess://' + base64(JSON.stringify(obj, null, 2));
-
 }
